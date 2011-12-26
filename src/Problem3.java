29a3033f5564703f753a5179b7e22ec6abac7326
Dimitrios Vlastaras <mail@dimme.net>
2011-12-26 09:47:44 +0100

    Problem 3 solved

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,25 +1,23 @@
-import java.util.*;
-
 public class Problem3 {
 
 	public static void main(String[] args) {
 		final long NUMBER = 600851475143L;
 		long max = 0;
-		
-		for (long i = 3; i<Math.sqrt(NUMBER); i+=2) {
+
+		for (long i = 3; i < Math.sqrt(NUMBER); i += 2) {
 			if (NUMBER % i == 0 && isPrime(i))
 				max = i;
 		}
-		
+
 		System.out.println(max);
 	}
-	
+
 	public static boolean isPrime(long num) {
-		for (long i = 3; i < Math.sqrt(num); i+=2)
+		for (long i = 3; i < Math.sqrt(num); i += 2)
 			if (num % i == 0)
 				return false;
-			
+
 		return true;
 	}
-	
+
 }
