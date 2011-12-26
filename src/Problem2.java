b26284d41780b3a7985cbbef0b80c729ea732297
Dimitrios Vlastaras <mail@dimme.net>
2011-12-26 08:11:52 +0100

    Problem 2 solved

---
+++ src/Problem2.java
@@ -0,0 +1,21 @@
+public class Problem2 {
+	public static void main(String[] args) {
+		
+		int beforeBefore = 1;
+		int before = 2;
+		int fib = 0;
+		int sum = 2;
+		
+		while ((fib = beforeBefore + before) <= 4000000) {
+			
+			beforeBefore = before;
+			before = fib;
+			
+			if (fib % 2 == 0) {
+				sum += fib;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+}
